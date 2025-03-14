6e6cdb1ec9989cfb1813163d1a0e63974eee1cca
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2025-03-13 22:09:40 -0300

    Added bottom buttons

--- app/src/main/java/com/ongs/voluntariar/screens/OrgInfoScreen.kt
+++ app/src/main/java/com/ongs/voluntariar/screens/OrgInfoScreen.kt
@@ -1,6 +1,7 @@
 package com.ongs.voluntariar.screens
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,17 +13,16 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,62 +44,58 @@ fun OrgInfoScreen(navController: NavController?){
     ) {
         Column(
             modifier = Modifier
-                .fillMaxSize(),
+                .fillMaxSize()
+                .background(color = Color(0xFFF5F5F5)),
             verticalArrangement = Arrangement.SpaceBetween,
             horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Box(
-                modifier = Modifier
-                    .background(color = Color(0xffaaaaaa))
-                    .padding(top = 100.dp, bottom = 2.dp)
-
-            )
-            Card (
+    ) {
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = "Ong Image",
                 modifier = Modifier
                     .fillMaxWidth()
-
-            ){}
-            Card (
-                modifier = Modifier
-                    .fillMaxSize()
-                    .height(500.dp),
-                shape = RoundedCornerShape(
-                    topStart = 30.dp,
-                    topEnd = 30.dp,
-                )
-            ) {
-                Text(
-                    text = stringResource(R.string.welcome),
-                    fontSize = 32.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold
-                )
+                    .clip(RoundedCornerShape(bottomEnd = 16.dp, bottomStart = 16.dp))
+            )
                 Column (
                     Modifier
                         .fillMaxSize()
                         .padding(20.dp),
                     verticalArrangement = Arrangement.SpaceBetween,
-                    horizontalAlignment = Alignment.End,
                 ){
-                    Column {
-                        Modifier.
-                        fillMaxWidth()
-                        Text(
-                            text = stringResource(R.string.your_name),
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.Bold
-                        )
-                        TextField(
-                            value = yourNameState.value,
-                            onValueChange = {
-                                yourNameState.value = it
-                            },
-                            modifier = Modifier.fillMaxWidth(),
-                            label = {
-                                Text(text = stringResource(R.string.your_name))
-                            }
-
-                        )
+                    Column (
+                        verticalArrangement = Arrangement.spacedBy(30.dp)
+                    ){
+                        Column {
+                            Modifier
+                                .fillMaxWidth()
+                            Text(
+                                text = "Ong Ser e Crescer",
+                                fontSize = 24.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(
+                                text = "Rua Barão de Campinas - Campinas-SP,\n 13101-180",
+                                fontSize = 12.sp,
+                                modifier = Modifier
+                                    .padding(0.dp, 5.dp, 0.dp, 0.dp)
+                            )
+                        }
+                        Column(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            Text(
+                                text = "About",
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold
+                            )
+                            Text(
+                                text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate \n",
+                                fontSize = 15.sp,
+                                modifier = Modifier
+                                    .padding(0.dp, 10.dp, 0.dp, 0.dp)
+                            )
+                        }
                     }
                     Column (
                         modifier = Modifier
@@ -126,8 +122,6 @@ fun OrgInfoScreen(navController: NavController?){
                             Text(text = "Contact")
                         }
                     }
-
-                }
             }
             /*Button(onClick = {}) {
                 Text(
